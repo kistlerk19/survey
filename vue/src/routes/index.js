9342be719d476c2c8f30575f8caf7a2aaef0930f
kistlerk19 <isgyamfi25@gmail.com>
2023-06-01 20:35:28 +0000

    auth redirect complete

--- vue/src/routes/index.js
+++ vue/src/routes/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DefaultLayout from "../components/DefaultLayout.vue"
+import AuthLayout from "../components/AuthLayout.vue"
 import Dashboard from "../views/Dashboard.vue"
 import Login from "../views/Login.vue"
 import Register from "../views/Register.vue"
@@ -27,15 +28,25 @@ const routes = [
     ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    meta: {isGuest: true},
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register
+      },
+    ]
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
+
 ];
 
 const router = createRouter({
@@ -46,7 +57,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth && !store.state.user.token) {
     next({ name: 'Login' })
-  } else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')) {
+  } else if (store.state.user.token && (to.meta.isGuest)) {
     next({ name: 'Dashboard' })
   } else {
     next()
