3eeb80b6f8d07dcd95594fe84debafa1d30cb323
kistlerk19 <isgyamfi25@gmail.com>
2023-06-02 19:50:27 +0000

    register full auth finished

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -6,17 +6,17 @@
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form class="space-y-6" action="#" method="POST">
+      <form class="space-y-6" @submit="register">
         <div>
           <label for="fullName" class="block text-sm font-medium leading-6 text-gray-900">Full Name</label>
           <div class="mt-2">
-            <input id="fullName" name="name" type="text" autocomplete="name" required="" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="fullName" name="name" type="text" autocomplete="name" required="" v-model="user.name" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="email" name="email" type="email" autocomplete="email" required="" v-model="user.email" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
@@ -25,15 +25,15 @@
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
           <div class="mt-2">
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="user.password" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
         <div>
           <div class="flex items-center justify-between">
-            <label for="confirm_password" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
+            <label for="password_confirmation" class="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
           </div>
           <div class="mt-2">
-            <input id="confirm_password" name="confirm_password" type="password" autocomplete="current-password" required="" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input id="password_confirmation" name="password_confirmation" type="password" required="" v-model="user.password_confirmation" class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
@@ -49,3 +49,29 @@
       </p>
     </div>
 </template>
+
+
+<script setup>
+  import { LockClosedIcon } from '@heroicons/vue/24/outline';
+  import store from '../store';
+  import { useRouter } from 'vue-router';
+
+  const router = useRouter();
+  const user = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  };
+
+  function register(ev) {
+    ev.preventDefault();
+    store
+      .dispatch('register', user)
+      .then((res) => {
+        router.push({
+          name: 'Dashboard'
+        })
+      })
+  }
+</script>
